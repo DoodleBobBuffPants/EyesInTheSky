a349799e2707c86be17403e5e189c1d2165e7cbf
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-01-31 20:24:00 +0000

    tested bebop import/able to start video stream/need to handle receipt

--- backend/VideoRetrieval.py
+++ backend/VideoRetrieval.py
@@ -1,1 +1,13 @@
 #Receive video from drone and pass on to movement modules
+import pyparrot.Bebop
+
+print("Starting stream") 	#starttest
+#Bebop.start_video_stream()
+
+#drone sends rtp packets
+#need to find a way to save them
+#and process them into video
+
+#alternatively VLC player will automatically play the video
+
+print("Exiting stream")		#endtest
