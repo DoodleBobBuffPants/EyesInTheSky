fe69c5d866ac1c7984b9b7ec5a26cd5549f862dd
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-02-02 21:37:53 +0000

    updated comments stating what is to be done

--- backend/VideoRetrieval.py
+++ backend/VideoRetrieval.py
@@ -8,13 +8,10 @@ print("Starting stream") 	#starttest
 #need to find a way to save them
 #and process them into video
 
-#alternatively VLC player will automatically play the video
+#can use opencv to process video by fram
+#need to find a way to display video in a browser
 
-
-#Need to start VLC player from python with an SDP file that handles reading packets from the drone
-#Then open a browser at localhost:8080 to display video stream there
-
-#Might be best to use this file as a starting point:
+#might be best to use this file as a starting point:
 # - start video feed from drone
 # - open player
 # - take frames and process them to get relative coordinates
