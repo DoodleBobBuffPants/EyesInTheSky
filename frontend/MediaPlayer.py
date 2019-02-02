2fe9a4bb05338d5d3765cee0b73dd9b5eceeffb6
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-02-02 21:38:24 +0000

    created video output using opencv

---
+++ frontend/MediaPlayer.py
@@ -0,0 +1,24 @@
+#read frames using opencv and display frames as a video
+import cv2 as cv
+
+#open video reader
+vidpath = "Pikachu Dango english version dub.mp4"		#test video - should use rtp://localhost:8080
+vc = cv.VideoCapture(vidpath)
+
+#loop through each frame, making hand over for analysis easier
+while(vc.isOpened()):
+	#successful read and frame
+	success, frame = vc.read()
+	if success:
+		#display frame sequence
+  		cv.imshow('Video', frame)
+  		cv.imwrite("frame.jpg", frame)
+  		#close 'video' on key input 'q'
+  		if (cv.waitKey(25) & 0xFF) == ord('q'):
+  			break
+	else:
+		break
+
+#release resources
+vc.release()
+cv.destroyAllWindows()
