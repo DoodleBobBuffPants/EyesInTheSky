966070cc20e76cf4658264e5e81dc9f8a293daaa
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-02-03 19:55:59 +0000

    completed more test cases

--- frontend/MediaPlayerTest.py
+++ frontend/MediaPlayerTest.py
@@ -1,4 +1,4 @@
 #get function to test
 import MediaPlayer as mp
-testvid = "testvideo.mp4"		#test video - should use rtp://localhost:8080
+testvid = "testvideo.mp4"		#test video - should use rtp://localhost:55004
 mp.playVid(testvid)
