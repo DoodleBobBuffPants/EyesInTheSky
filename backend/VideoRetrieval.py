0cb8c35d9960aa15e5de3ce637324aee50c8829a
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-02-03 18:09:31 +0000

    initiate player in a new thread

--- backend/VideoRetrieval.py
+++ backend/VideoRetrieval.py
@@ -1,23 +1,23 @@
-#Initiate drone video stream and media player
+#initiate drone video stream and media player
+#must start execution from folder containing this file
 import sys
 sys.path.append("..")
+from threading import Thread
 import pyparrot.Bebop		#library containing drone commands
 import frontend.MediaPlayer as mp
 
 print("Starting stream") 	#starttest
 #Bebop.start_video_stream()
-mp.playVid("../frontend/testvideo.mp4")
 
-#drone sends rtp packets
-#need to find a way to save them
-#and process them into video
+#start video stream as separate process as it is blocking
+vidPath = "../frontend/testvideo.mp4"
+streamProc = Thread(target = mp.playVid, args = [vidPath])
+streamProc.start()
 
-#can use opencv to process video by fram
 #need to find a way to display video in a browser
-
 #might be best to use this file as a starting point:
 # - start video feed from drone
-# - open player
+# - open player in separate thread
 # - take frames and process them to get relative coordinates
 # - give to movement module on request (will be concurrent so need threadings)
 
