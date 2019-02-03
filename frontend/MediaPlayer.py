f69e7482650866c9079c3fb49a72e3af305cf00d
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-02-03 19:43:04 +0000

    added custom naive lock for file access

--- frontend/MediaPlayer.py
+++ frontend/MediaPlayer.py
@@ -1,6 +1,9 @@
 #read frames using opencv and display frames as a video
+import sys
+sys.path.append("..")
 import cv2 as cv
-from filelock import FileLock
+#from filelock import FileLock
+import backend.Lock as lock 		#custom naive lock
 def playVid(vidpath):
 	vc = cv.VideoCapture(vidpath)
 	#loop through each frame, making hand over for analysis easier
@@ -10,8 +13,9 @@ def playVid(vidpath):
 		if success:
 			#display frame sequence
 			cv.imshow('Video', frame)
-			with FileLock("frame.jpg"):
-				cv.imwrite("frame.jpg", frame)
+			lock.take_lock()
+			cv.imwrite("frame.jpg", frame)
+			lock.release_lock()
 			#close 'video' on key input 'q'
 			if (cv.waitKey(7) == ord('q')):
 				break
