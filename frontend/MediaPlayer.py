c17a0ece63106ea513bcd20cef074fd0d46eb223
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-02-03 18:33:53 +0000

    added concurrency control on written frame

--- frontend/MediaPlayer.py
+++ frontend/MediaPlayer.py
@@ -1,20 +1,20 @@
 #read frames using opencv and display frames as a video
 import cv2 as cv
+from filelock import FileLock
 def playVid(vidpath):
 	vc = cv.VideoCapture(vidpath)
 	#loop through each frame, making hand over for analysis easier
-	while(vc.isOpened()):
+	while True:
 		#successful read and frame
 		success, frame = vc.read()
 		if success:
 			#display frame sequence
-	  		cv.imshow('Video', frame)
-	  		cv.imwrite("frame.jpg", frame)
-	  		#close 'video' on key input 'q'
-	  		if (cv.waitKey(25) & 0xFF) == ord('q'):
-	  			break
-		else:
-			break
+			cv.imshow('Video', frame)
+			with FileLock("frame.jpg"):
+				cv.imwrite("frame.jpg", frame)
+			#close 'video' on key input 'q'
+			if (cv.waitKey(7) == ord('q')):
+				break
 	#release resources
 	vc.release()
 	cv.destroyAllWindows()
