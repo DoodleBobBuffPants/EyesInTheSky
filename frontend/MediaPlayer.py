3eb77647e296b1bd09fa41097635ff189bf4364d
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-02-05 19:41:12 +0000

    added more multithreading to reduce latency in frames

--- frontend/MediaPlayer.py
+++ frontend/MediaPlayer.py
@@ -1,25 +1,27 @@
-# read frames using opencv and display frames as a video
+ # read frames using opencv and display frames as a video
 import sys
 sys.path.append("..")
 import cv2 as cv
-# from filelock import FileLock
+from threading import Thread
+import queue.Queue as Queue
 import backend.Lock as lock 		# custom naive lock
+import FrameGetter as fg
 def playVid(vidpath, bebop):
-	# start stream here to reduce latency
-	bebop.start_video_stream()
-	vc = cv.VideoCapture(vidpath)
+	# queue of frames
+	queue = new Queue()
+	# new thread to get frames concurrently
+	fgProc = Thread(target = fg.frameGetter(), args = [queue, bebop, vidpath])
+	fgProc.start()
 	# loop through each frame, making hand over for analysis easier
 	while True:
-		# successful read and frame
-		success, frame = vc.read()
-		if success:
-			# display frame sequence
-			cv.imshow('Video', frame)
-			# synchronised write out of frame for concurrency control during analysis
-			lock.take_lock()
-			cv.imwrite("frame.jpg", frame)
-			lock.release_lock()
-			cv.waitKey(1)
+		# read frame
+		frame = queue.get()
+		cv.imshow('Video', frame)
+		# synchronised write out of frame for concurrency control during analysis
+		lock.take_lock()
+		cv.imwrite("frame.jpg", frame)
+		lock.release_lock()
+		cv.waitKey(1)
 	# release resources
 	vc.release()
 	cv.destroyAllWindows()
