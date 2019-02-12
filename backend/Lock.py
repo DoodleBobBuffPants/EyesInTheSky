e3a1cac17ede6dbf12493ed714a58213f618dc05
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-02-12 10:38:43 +0000

    PEP8

--- backend/Lock.py
+++ backend/Lock.py
@@ -1,15 +1,15 @@
 '''naive lock, but works for our use'''
-#lock variable
+# lock variable
 lock = 0
-#take lock
+# take lock
 def take_lock():
-	#spin while taken
+	# spin while taken
 	try:
 		while lock == 1:
 			pass
 		lock = 1
 	except NameError:
 		lock = 1
-#release lock
+# release lock
 def release_lock():
 	lock = 0
