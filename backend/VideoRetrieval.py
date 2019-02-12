c05a330fdcc122770376c58242249145448f5bd7
Anik Roy <anik545@gmail.com>
2019-02-12 13:24:55 +0000

    refomatting

--- backend/VideoRetrieval.py
+++ backend/VideoRetrieval.py
@@ -1,17 +1,18 @@
 # initiate drone video stream and media player
 # must start execution from folder containing this file
 import sys
+
 sys.path.append("..")
 from threading import Thread
-from pyparrot.Bebop import Bebop		# library containing drone commands
+from pyparrot.Bebop import Bebop  # library containing drone commands
 import frontend.MediaPlayer as mp
 
 bebop = Bebop()
 bebop.connect(10)
-bebop.set_video_resolutions("rec1080_stream480")
-bebop.set_video_framerate("24_FPS")
+# bebop.set_video_resolutions("rec1080_stream480")
+# bebop.set_video_framerate("24_FPS")
 
 # start video stream as separate process as it is blocking
 vidPath = "../frontend/bebop.sdp"
-streamProc = Thread(target = mp.playVid, args = [vidPath, bebop])
+streamProc = Thread(target=mp.playVid, args=[vidPath, bebop])
 streamProc.start()
