2366dbc68b73d79112bfa174d2642f3a25675d6e
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-02-21 14:52:59 +0000

    save frame for vlc-based player for non windows

--- backend/server/main.py
+++ backend/server/main.py
@@ -8,17 +8,14 @@ drone = App.drone
 
 class UserVision:
     def __init__(self, vision):
-        self.index = 0
         self.vision = vision
 
     def save_pictures(self, args):
-        #print("saving picture")
+        # print("saving picture")
         img = self.vision.get_latest_valid_picture()
 
         if (img is not None):
-            filename = "test_image_%06d.png" % self.index
-            #cv2.imwrite(filename, img)
-            self.index +=1
+            cv2.imwrite("frame_vlc.jpg", img)
 
 
 # Launch the web server in seperate thread.
