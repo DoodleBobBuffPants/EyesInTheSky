d086d80fdd0b3633003f600f7ef8f2e0b039bf07
fmg30 <fmg30@cam.ac.uk>
2019-02-25 09:36:12 +0000

    delete some prints

--- backend/CallCarFilter.py
+++ backend/CallCarFilter.py
@@ -1,7 +1,6 @@
 # call CarFilterFrame.m using one frame at a time
 
 # TODO: test from app again
-# TODO: read from frame_vlc
 # TODO: start matlab engine earlier
 import matlab.engine
 import cv2 as cv
@@ -28,32 +27,24 @@ def call_car_filter(bebop, lock, source='drone'):
     while True:
         # write the frame for the filter to read from
         cv.imwrite("backend/frame_for_filter.jpg", frame)
-        print("updated picture")
         # run the car filter with current frame
         a = eng.run(cf, "../frame_for_filter.jpg")
 
         # if the filter returns any centroids update bebop
         if len(a) > 0:
             x, y = coords_from_centroid(a[0], width, height)
-            # bebop.update_coords(x, y)
-            print(x, y)
-        print("loading next frame")
+            bebop.update_coords(x, y)
         frame, vc = load_frame(bebop, lock, source, vc)
-        print("got next frame")
 
 
 def load_frame(bebop, lock, source, vc=None):
     # load a frame from either the drone or an mp4
     if source == 'drone':
-        print("taking lock")
         lock.take_lock()
-        print("got lock")
         frame = None
         while frame is None:
             frame = cv.imread("frame.jpg")
-        print("releasing lock")
         lock.release_lock()
-        print("returning")
         return frame, vc
     elif source == 'mp4':
         if vc is None: # set up video capture 
