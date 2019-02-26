dc6156b4f34a148ecf195eab0a3693e8a77b3576
fmg30 <fmg30@cam.ac.uk>
2019-02-26 08:38:23 +0000

    working as separate call

--- backend/callOriginalFilter.py
+++ backend/callOriginalFilter.py
@@ -1,7 +1,8 @@
 # function to check the original filter function for comparison
 # simple test to call CarFilter.m from Python
+# run from this folder
 
 import matlab.engine
 eng = matlab.engine.start_matlab()
-cf = eng.CarFilter(nargout=0) # number of args returned from matlab
-a = eng.run(cf)
+eng.CarFilter(nargout=0) # number of args returned from matlab
+
